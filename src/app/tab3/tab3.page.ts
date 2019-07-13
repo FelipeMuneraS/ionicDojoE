491f3585afb9a9551bb7756f34e1c897579faa32
Felipe <luisf.munera@udea.edu.co>
2019-07-13 09:19:29 -0500

    buscar foto

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -25,4 +25,18 @@ export class Tab3Page {
       this.foto = 'data:image/jpeg;base64,' + dataImage;
     });
   }
+
+  buscarFoto() {
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    }).then(dataImage => {
+      this.foto = 'data:image/jpeg;base64,' + dataImage;
+    });
+  }
 }
